7fd58642eb7daa603b30b970cfaf657c5222ef6b
Saketh <alurusaisaketh@gmail.com>
2020-04-14 11:46:06 +0530

    Added files from bert_codes used for cnn_gru

--- CNN_GRU/bert_codes/feature_generation.py
+++ CNN_GRU/bert_codes/feature_generation.py
@@ -23,7 +23,7 @@ def custom_tokenize(sentences,tokenizer,max_length=512):
         try:
 
             encoded_sent = tokenizer.encode(
-                                sent,                      # Sentence to encode.
+                                sent ,                     # Sentence to encode.
                                 add_special_tokens = True, # Add '[CLS]' and '[SEP]'
                                 max_length = max_length,
                                 # This function also supports truncation and conversion
@@ -31,6 +31,7 @@ def custom_tokenize(sentences,tokenizer,max_length=512):
                                 # can't use these features :( .
                                 #max_length = 128,          # Truncate all sentences.
                                 #return_tensors = 'pt',     # Return pytorch tensors.
+                           
                            )
 
             # Add the encoded sentence to the list.
@@ -52,6 +53,95 @@ def custom_tokenize(sentences,tokenizer,max_length=512):
 
     return input_ids
 
+
+
+####### pair of sentences 
+def custom_tokenize_pair(sentences,tokenizer,max_length=512):
+    input_ids = []
+    # For every sentence...
+    for sent in sentences:
+        # `encode` will:
+        #   (1) Tokenize the sentence.
+        #   (2) Prepend the `[CLS]` token to the start.
+        #   (3) Append the `[SEP]` token to the end.
+        #   (4) Map tokens to their IDs.
+        try:
+
+            encoded_sent = tokenizer.encode(
+                                sent[0],  
+                                sent[1],                    # Sentence to encode.
+                                add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                                max_length = max_length,
+                                # This function also supports truncation and conversion
+                                # to pytorch tensors, but we need to do padding, so we
+                                # can't use these features :( .
+                                #max_length = 128,          # Truncate all sentences.
+                                #return_tensors = 'pt',     # Return pytorch tensors.
+                           
+                           )
+
+            # Add the encoded sentence to the list.
+            
+        except ValueError:
+            encoded_sent = tokenizer.encode(
+                                ' ',                      # Sentence to encode.
+                                ' ',
+                                add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                                max_length = max_length,
+                                # This function also supports truncation and conversion
+                                # to pytorch tensors, but we need to do padding, so we
+                                # can't use these features :( .
+                                #max_length = 128,          # Truncate all sentences.
+                                #return_tensors = 'pt',     # Return pytorch tensors.
+                           )
+              ### decide what to later
+        
+        input_ids.append(encoded_sent)
+
+    return input_ids
+
+
+def custom_tokenize_pair_two(sentences,tokenizer,max_length=512):
+    ml_sent1=int(max_length*2/3)
+    ml_sent2=int(max_length*1/3)
+    
+    print('length of sent', ml_sent1,ml_sent2)
+    
+    sentences_0=[sent[0] for sent in sentences]
+    sentences_1=[sent[1] for sent in sentences]
+    
+    
+    
+    encode_sent1=custom_tokenize(sentences_0,tokenizer,ml_sent1)
+    encode_sent2=custom_tokenize(sentences_1,tokenizer,ml_sent2)
+    
+    print('encoded sent', encode_sent1[0])
+    input_ids=[]
+    for sent1,sent2 in zip(encode_sent1,encode_sent2):
+        sent=list(sent1)+list(sent2)
+        input_ids.append(sent)
+    return input_ids
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def custom_att_masks(input_ids):
     attention_masks = []
 
@@ -67,8 +157,14 @@ def custom_att_masks(input_ids):
         attention_masks.append(att_mask)
     return attention_masks
 
-def combine_features(sentences,tokenizer,max_length=512):
-    input_ids=custom_tokenize(sentences,tokenizer,max_length)
+def combine_features(sentences,tokenizer,max_length=512, take_pair=True,take_target=False):
+    if(take_pair):
+        input_ids=custom_tokenize_pair_two(sentences,tokenizer,max_length)
+    elif(take_target):
+        input_ids=custom_tokenize_pair(sentences,tokenizer,max_length)
+    else:
+        input_ids=custom_tokenize(sentences,tokenizer,max_length)
+    print('Input shape before truncating',input_ids[0:5])
     input_ids = pad_sequences(input_ids, dtype="long", 
                           value=0, truncating="post", padding="post")
     print(input_ids.shape)
@@ -80,6 +176,21 @@ def return_dataloader(input_ids,labels,att_masks,batch_size=8,is_train=False):
     labels = torch.tensor(labels,dtype=torch.long)
     masks = torch.tensor(np.array(att_masks))
     data = TensorDataset(inputs, masks, labels)
+    if(is_train==False):
+        sampler = SequentialSampler(data)
+    else:
+        sampler = RandomSampler(data)
+    dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+    return dataloader
+
+def return_cnngru_dataloader(tuples,batch_size=8,is_train=False):
+
+    input_ids=[ele[0] for ele in tuples]
+    labels=[ele[1] for ele in tuples]
+
+    inputs = torch.tensor(input_ids)
+    labels = torch.tensor(labels,dtype=torch.long)
+    data = TensorDataset(inputs, labels)
     if(is_train==False):
         sampler = SequentialSampler(data)
     else:
--- CNN_GRU/bert_codes/own_bert_models.py
+++ CNN_GRU/bert_codes/own_bert_models.py
@@ -2,29 +2,40 @@ from transformers.modeling_bert import *
 
 
 
-def select_model(type_of_model,path,weights=None,label_list=None):
+def select_model(type_of_model,path,weights=None,label_list=None,num_labels = 2):
     if(type_of_model=='weighted'):
         model = SC_weighted_BERT.from_pretrained(
         path, # Use the 12-layer BERT model, with an uncased vocab.
-        num_labels = 2, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
+        num_labels = num_labels, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
         output_attentions = False, # Whether the model returns attentions weights.
         output_hidden_states = False, # Whether the model returns all hidden-states.
+
         weights=weights
     )
     elif(type_of_model=='normal'):
         model = BertForSequenceClassification.from_pretrained(
           path, # Use the 12-layer BERT model, with an uncased vocab.
-          num_labels = 2, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
+          num_labels = num_labels, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
           output_attentions = False, # Whether the model returns attentions weights.
           output_hidden_states = False, # Whether the model returns all hidden-states.
+          hidden_dropout_prob=0.3
         )
     elif(type_of_model=='multitask'):
         model = BertForMultitask.from_pretrained(
           path, # Use the 12-layer BERT model, with an uncased vocab.
-          num_labels = 2, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
+          num_labels = num_labels, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
           output_attentions = False, # Whether the model returns attentions weights.
           output_hidden_states = False, # Whether the model returns all hidden-states.
           label_uniques=label_list
+        )
+    elif(type_of_model=='normal_softmax'):
+        model = SC_weighted_BERT_Softmax.from_pretrained(
+            path, # Use the 12-layer BERT model, with an uncased vocab.
+            num_labels = num_labels, # The number of output labels--2 for binary classification             # You can increase this for multi-class tasks.   
+            output_attentions = False, # Whether the model returns attentions weights.
+            output_hidden_states = False, # Whether the model returns all hidden-states.
+            # hidden_dropout_prob = 0.3,
+            weights = weights
         )
     else:
         print("Error in model name!!!!")
@@ -46,7 +57,7 @@ class SC_weighted_BERT(BertPreTrainedModel):
         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
         self.init_weights()
 
-    def forward(
+    def forward(hidden_size,
         self,
         input_ids=None,
         attention_mask=None,
@@ -84,6 +95,8 @@ class SC_weighted_BERT(BertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
+
+
 
 
 class BertForMultitask(BertPreTrainedModel):
@@ -129,13 +142,23 @@ class BertForMultitask(BertPreTrainedModel):
             logits = self.classifier_list[i](output_1)
             logits_list.append(logits)
         outputs = (logits_list,) + outputs[2:]  # add hidden states and attention if they are here
-        loss=0
-        for i in range(len(self.label_uniques)):
-            # label=torch.nn.functional.one_hot(labels[:,i])
-            label=labels[:,i]
+
+        if labels is not None:
+            loss=0
+            # for i in range(len(self.label_uniques)):
+            #     # label=torch.nn.functional.one_hot(labels[:,i])
+
+            #     label=labels[:,i]
+            #     loss_fct = CrossEntropyLoss(reduction='mean').cuda()
+            #     loss += loss_fct(logits_list[i].view(-1, self.label_uniques[i]), label.view(-1))
+            label = labels[:,0]
             loss_fct = CrossEntropyLoss(reduction='mean').cuda()
-            loss += loss_fct(logits_list[i].view(-1, self.label_uniques[i]), label.view(-1))
-        outputs = (loss,) + outputs
+            loss += loss_fct(logits_list[0].view(-1, self.label_uniques[0]), label.view(-1))
+
+            targets = labels[:,1:]
+            loss_bce = nn.BCEWithLogitsLoss().cuda()
+            loss += loss_bce(logits_list[1].view(-1,self.label_uniques[1]).float(),targets.view(-1,self.label_uniques[1]).float())
+            outputs = (loss,) + outputs
 
 
 
@@ -149,5 +172,58 @@ class BertForMultitask(BertPreTrainedModel):
         #         loss_fct = CrossEntropyLoss(weight=torch.tensor(self.weights).cuda())
         #         loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
         #     outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+
+class SC_weighted_BERT_Softmax(BertPreTrainedModel):   
+    def __init__(self, config,weights):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.softmax = nn.Softmax(dim=1)
+        self.init_weights()
+
+    def forward(self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+    ):
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        logits = self.softmax(logits)
+        # print('logit shape',logits.shape)
+        # print('labels',labels.shape)
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+                # print('logits.view(-1, self.num_labels)',logits.view(-1, self.num_labels).shape)
+                # print('labels.view(-1)',labels.view(-1).shape)
+                # assert False
+            outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
